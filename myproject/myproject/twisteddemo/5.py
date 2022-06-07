86f1e1c1be267f770879e1a9e00747c954636221
chenxiangan <1598828268@qq.com>
2022-06-07 22:21:12 +0800

    Converting Twisted’s inlineCallbacks to async

--- myproject/myproject/twisteddemo/5.py
+++ myproject/myproject/twisteddemo/5.py
@@ -1,44 +1,85 @@
-# -*- coding: utf-8 -*-
-# @Time : 2022/6/7 9:27 下午
-# @Author : chenxiangan
-# @File : 5.py
-# @Software: PyCharm
-"""
-内联回调
-"""
-from twisted.internet import reactor,defer
+# # -*- coding: utf-8 -*-
+# # @Time : 2022/6/7 9:27 下午
+# # @Author : chenxiangan
+# # @File : 5.py
+# # @Software: PyCharm
+# """
+# 内联回调
+# """
+# from twisted.internet import reactor,defer
+#
+#
+# def callback(num):
+#     print("callback called with num =", num)
+#     deferred = defer.Deferred()
+#     #deferred.callback(num+1)
+#     reactor.callLater(3,deferred.callback, num+1)
+#     return deferred
+#
+#
+# def callback2(num):
+#     print("callback2 called with num =", num)
+#     deferred = defer.Deferred()
+#     deferred.callback(num+1)
+#     return deferred
+#
+# def callback3(num):
+#     print("callback3 called with num =", num)
+#     deferred = defer.Deferred()
+#     reactor.callLater(2,deferred.callback, num+1)
+#     return deferred
+# @defer.inlineCallbacks
+# def main():
+#     result = yield callback(5)
+#     result2 = yield callback2(6)
+#     result3 = yield callback2(7)
+#     print(result)
+#     print(result2)
+#     print(result3)
+#     reactor.stop()
+#
+# if __name__ == '__main__':
+#     print("twisted开始运行")
+#     reactor.callWhenRunning(main)
+#     reactor.run()
+
+#对于Python3可以使用async/await
+from twisted.internet import reactor, defer
 
 
 def callback(num):
     print("callback called with num =", num)
     deferred = defer.Deferred()
-    #deferred.callback(num+1)
-    reactor.callLater(3,deferred.callback, num+1)
+    # deferred.callback(num+1)
+    reactor.callLater(3, deferred.callback, num + 1)
     return deferred
 
 
 def callback2(num):
     print("callback2 called with num =", num)
     deferred = defer.Deferred()
-    deferred.callback(num+1)
+    deferred.callback(num + 1)
     return deferred
+
 
 def callback3(num):
     print("callback3 called with num =", num)
     deferred = defer.Deferred()
-    reactor.callLater(2,deferred.callback, num+1)
+    reactor.callLater(2, deferred.callback, num + 1)
     return deferred
-@defer.inlineCallbacks
-def main():
-    result = yield callback(5)
-    result2 = yield callback2(6)
-    result3 = yield callback2(7)
+
+
+async def main():
+    result = await callback(5)
+    result2 = await callback2(6)
+    result3 = await callback2(7)
     print(result)
     print(result2)
     print(result3)
     reactor.stop()
 
+
 if __name__ == '__main__':
     print("twisted开始运行")
-    reactor.callWhenRunning(main)
+    defer.ensureDeferred(main())
     reactor.run()
